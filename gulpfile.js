82c3a7e990cbdb0b82ddd293540f27d4bf1b4c8f
Carl Topham <carl@carl-topham.com>
2017-10-09 21:08:23 +0100

    Added initial files

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+var SRC_SCSS = './src/scss/**/*.scss';
+var DEST_CSS = './static/css';
+
+var post_css_plugins = [
+    autoprefixer({browsers: ['last 3 version']})
+];
+
+gulp.task('sass', function () {
+  return gulp.src(SRC_SCSS)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(post_css_plugins))
+    .pipe(gulp.dest(DEST_CSS));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(SRC_SCSS, ['sass']);
+});
+
+gulp.task('default', ['sass']);
