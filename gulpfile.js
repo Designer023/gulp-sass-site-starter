8acce4602a6ad385f5a662c6cff940050a9db880
Carl Topham <carl@carl-topham.com>
2017-10-10 07:58:00 +0100

    Documentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 'use strict';
 
+// Import packages
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 
+// Define sources and destinations
 var SRC_SCSS = './src/scss/**/*.scss';
 var DEST_CSS = './static/css';
 
+// Configure PostCSS plugins
 var post_css_plugins = [
     autoprefixer({browsers: ['last 3 version']})
 ];
 
+//
 gulp.task('sass', function () {
   return gulp.src(SRC_SCSS)
     .pipe(sass().on('error', sass.logError))
@@ -19,8 +23,10 @@ gulp.task('sass', function () {
     .pipe(gulp.dest(DEST_CSS));
 });
 
+
 gulp.task('watch', function () {
   gulp.watch(SRC_SCSS, ['sass']);
 });
 
+// 
 gulp.task('default', ['sass']);
